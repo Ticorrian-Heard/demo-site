72fcf72efd40974b95bd781a77d784db9fce2241
Ticorrian Heard <ticorrian.heard@zoom.us$>
2023-09-29 14:08:17 -0400

    Latest

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports={
         host: 'localhost',
         liveReload:true,
         open: true,
+        allowedHosts: 'all',
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',

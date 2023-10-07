762c0b65669ed0eadbf0be2b9250aa4c75792aa0
Ticorrian Heard <ticorrian.heard@zoom.us$>
2023-10-06 23:37:20 -0400

    latest

--- src/components/HerosSection.jsx
+++ src/components/HerosSection.jsx
@@ -8,7 +8,7 @@ function HerosSection() {
         <video src='../../assets/videos/video-2.mp4' playsInline autoPlay loop muted></video>
         <h1>Welcome to our demo site!</h1>
         <div className='hero-btns'>
-            <Button path='https://explore.zoom.us/en/video-sdk/' buttonStyle='btn--primary' buttonSize='btn--large' extraStyle='transition-colors hover:bg-slate-300 active:bg-slate-500'>Get Started with the Zoom Video SDK</Button>
+            <Button path='https://explore.zoom.us/en/video-sdk/' buttonStyle='btn--primary' buttonSize='btn--large' extraStyle='transition-colors hover:bg-slate-300 active:bg-slate-500'>Get Started with Zoom's Video SDK</Button>
         </div>
     </div>
   )

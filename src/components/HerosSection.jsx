e36f81fada74c8547e6fcc8ddd3a5f72a38ce182
Ticorrian Heard <ticorrian.heard@zoom.us>
2023-10-18 23:56:57 -0400

    latest

--- src/components/HerosSection.jsx
+++ src/components/HerosSection.jsx
@@ -5,10 +5,10 @@ import './HeroSection.css';
 function HerosSection() {
   return (
     <div className='hero-container'>
-        <video src='../../assets/videos/video-2.mp4' playsInline autoPlay loop muted></video>
-        <h1>Welcome to our demo site!</h1>
+        <video src='../../assets/videos/bubbles.mov' playsInline autoPlay loop muted></video>
+        <h1>Build with Zoom’s Core Technology</h1>
         <div className='hero-btns'>
-            <Button path='https://explore.zoom.us/en/video-sdk/' buttonStyle='btn--primary' buttonSize='btn--large' extraStyle='transition-colors hover:bg-slate-300 active:bg-slate-500'>Get Started with Zoom's Video SDK</Button>
+            <Button path='https://explore.zoom.us/en/video-sdk/' buttonStyle='btn--transparent' buttonSize='btn--large' extraStyle='transition-colors hover:bg-slate-300 active:bg-slate-500'>Get Started with Zoom's Video SDK</Button>
         </div>
     </div>
   )
